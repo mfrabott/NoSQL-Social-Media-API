d143e775d29db2acd2bbb7be4170992639443609
mfrabott <mfrabott14@gmail.com>
2022-12-23 16:21:36 -0500

    add updaye user route

--- controllers/userController.js
+++ controllers/userController.js
@@ -38,6 +38,23 @@ module.exports = {
       .catch((err) => res.status(500).json(err));
   },
 
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with this id!' })
+          : res.json(user)
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
 
   // delete a user and remove thoughts
   deleteUser(req, res) {
